b14a18b3b9d2fefeb7a30f975e91487327c03122
henriquehbr <henriqueborgeshbr@gmail.com>
2020-03-09 10:39:34 -0300

    chore: add `example` directory for testing purposes

---
+++ example/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Typewriter from '../Typewriter'
+</script>
+
+<Typewriter>
+	<p>Hello World!</p>
+	<nav>
+		<a href='/'>Lorem ipsum</a>
+		<a href='/'>Dolor sit</a>
+		<a href='/'>Amet consectetur</a>
+	</nav>
+</Typewriter>
