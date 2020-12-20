5e004db140f82357dc7819400efaa2e013a45b9e
Henrique Borges <henriqueborgeshbr@gmail.com>
2020-12-20 13:58:56 -0300

    refactor: unify both cascade and default modes (#33)
    refactor(helpers): modularize cascade mode MutationOberver into
    "onAnimationEnd"

--- src/modes/index.ts
+++ src/modes/index.ts
@@ -4,9 +4,8 @@ import type { TypewriterMainFn } from '@svelte-typewriter/types'
 const typewriter: TypewriterMainFn = async (node, options) => {
 	const { default: mode } =
 		((options.loop || options.loopRandom) && (await import('./loop'))) ||
-		(options.cascade && (await import('./cascade'))) ||
 		(options.scramble && (await import('./scramble'))) ||
-		(await import('./default'))
+		(await import('./typewriter'))
 	const elements = getElements(node)
 	if (options.delay > 0) {
 		const { sleep } = await import('../helpers')
---
+++ src/modes/typewriter.ts
@@ -0,0 +1,22 @@
+import { writeEffect, onAnimationEnd } from '@svelte-typewriter/helpers'
+import type { TypewriterElement, TypewriterModeFn } from '@svelte-typewriter/types'
+
+type CleanChildText = (elements: TypewriterElement[]) => void
+
+const cleanChildText: CleanChildText = elements =>
+	elements.forEach(element => (element.currentNode.textContent = ''))
+
+const defaultMode: TypewriterModeFn = async ({ elements }, options) => {
+	if (options.cascade) {
+		cleanChildText(elements)
+	} else {
+		const { getLongestTextElement } = await import('@svelte-typewriter/helpers')
+		const lastElementToFinish = getLongestTextElement(elements)
+		onAnimationEnd(lastElementToFinish, () => options.dispatch('done'))
+	}
+	for (const element of elements)
+		options.cascade ? await writeEffect(element, options) : writeEffect(element, options)
+	options.cascade && options.dispatch('done')
+}
+
+export default defaultMode
