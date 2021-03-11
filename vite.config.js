cc03b485ffb4c9478935c979f4f2099b7cacbf54
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-03-11 08:28:32 -0300

    build: setup vite (#38)

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+import { defineConfig } from 'vite'
+import svelte from '@svitejs/vite-plugin-svelte'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig(({ mode }) => {
+	const isProduction = mode === 'production'
+	return {
+		build: {
+			lib: {
+				entry: './src/Typewriter.svelte',
+				formats: ['es', 'cjs']
+			},
+			outDir: './lib',
+			polyfillDynamicImport: false,
+			cssCodeSplit: false,
+			minify: isProduction
+		},
+		extensions: ['html', 'js', 'css'],
+		resolve: {
+			alias: {
+				svelte: resolve(__dirname, 'node_modules/svelte'),
+				'@svelte-typewriter': resolve(__dirname, 'src')
+			}
+		},
+		plugins: [
+			svelte({
+				hot: !isProduction,
+				emitCss: true
+			})
+		]
+	}
+})
