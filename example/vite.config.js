09a58ddb1c6edf901d440bdd5bd61004b09b26bd
Henrique Borges <henriqueborgeshbr@gmail.com>
2022-06-29 17:47:41 -0300

    chore: setup example application deploy process

--- example/vite.config.js
+++ example/vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  // deploys to "https://henriquehbr.github.io/svelte-typewriter"
+  base: "/svelte-typewriter/"
 })
