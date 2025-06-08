d0e4606752419463fd8d1f9f87e81900a7d2ee77
Nassim Tabchiche <n.tabchiche2@gmail.com>
2025-06-09 00:27:59 +0200

    migrate Typewriter to svelte 5

--- src/lib/Typewriter.svelte
+++ src/lib/Typewriter.svelte
@@ -1,67 +1,146 @@
-<!-- @migration-task Error while migrating Svelte code: $$props is used together with named props in a way that cannot be automatically migrated. -->
 <script>
-    import '@formatjs/intl-segmenter/polyfill'
+	import { run } from 'svelte/legacy'
 
-    export let mode = "concurrent"
+	import '@formatjs/intl-segmenter/polyfill'
 
-    // general-purpose props
-	export let interval = 30
-	export let cursor = true
-    export let keepCursorOnFinish = false
-	export let delay = 0
-    export let showCursorOnDelay = false
-    export let disabled = false
-    export let element = "div"
+	/**
+	 * @typedef {Object} Props
+	 * @property {string} [mode]
+	 * @property {number} [interval] - general-purpose props
+	 * @property {boolean} [cursor]
+	 * @property {boolean} [keepCursorOnFinish]
+	 * @property {number} [delay]
+	 * @property {boolean} [showCursorOnDelay]
+	 * @property {boolean} [disabled]
+	 * @property {string} [element]
+	 * @property {number} [scrambleDuration] - mode-specific props
+	 * @property {boolean} [scrambleSlowdown]
+	 * @property {number} [unwriteInterval]
+	 * @property {number} [wordInterval]
+	 * @property {import('svelte').Snippet} [children]
+	 */
 
-    // mode-specific props
-    export let scrambleDuration = 3000
-    export let scrambleSlowdown = true
-	export let unwriteInterval = 30
-    export let wordInterval = 1500
+	/** @type {Props} */
+	let {
+		mode = 'concurrent',
+		interval = 30,
+		cursor = true,
+		keepCursorOnFinish = false,
+		delay = 0,
+		showCursorOnDelay = false,
+		disabled = false,
+		element = 'div',
+		scrambleDuration = 3000,
+		scrambleSlowdown = true,
+		unwriteInterval = 30,
+		wordInterval = 1500,
+		children
+	} = $props()
 
-    $: isLoopMode = /^loop(Once|Random)?$/.test(mode)
+	let isLoopMode = $derived(/^loop(Once|Random)?$/.test(mode))
 
-    // these modes stop once all given elements have finished their animations
-    // and support the cursor
-    $: isFiniteCursorMode = ["concurrent", "cascade", "loopOnce"].includes(mode)
+	// these modes stop once all given elements have finished their animations
+	// and support the cursor
+	let isFiniteCursorMode = $derived(['concurrent', 'cascade', 'loopOnce'].includes(mode))
 
-    $: invalidCursorOnFinish = !isFiniteCursorMode && keepCursorOnFinish
-    $: invalidCursorOnDelay = delay < 1 && showCursorOnDelay
-    $: invalidLoopProps = !isLoopMode && ($$props.unwriteInterval || $$props.wordInterval)
-    $: invalidScrambleProps = mode !== "scramble" && ($$props.scrambleDuration || $$props.scrambleSlowdown)
-    $: unnecessaryCursorOnFinish = typeof keepCursorOnFinish === 'number' && keepCursorOnFinish < 1
+	let invalidCursorOnFinish = $derived(!isFiniteCursorMode && keepCursorOnFinish)
+	let invalidCursorOnDelay = $derived(delay < 1 && showCursorOnDelay)
+	let invalidLoopProps = $derived(!isLoopMode && (unwriteInterval || wordInterval))
+	let invalidScrambleProps = $derived(
+		mode !== 'scramble' && (scrambleDuration || scrambleSlowdown)
+	)
+	let unnecessaryCursorOnFinish = $derived(
+		typeof keepCursorOnFinish === 'number' && keepCursorOnFinish < 1
+	)
 
-    const modes = {
-        concurrent: () => import("./modes/concurrent"),
-        cascade: () => import("./modes/cascade"),
-        loop: () => import("./modes/loop"),
-        loopOnce: () => import("./modes/loopOnce"),
-        loopRandom: () => import("./modes/loopRandom"),
-        scramble: () => import("./modes/scramble")
-    }
+	const modes = {
+		concurrent: () => import('./modes/concurrent'),
+		cascade: () => import('./modes/cascade'),
+		loop: () => import('./modes/loop'),
+		loopOnce: () => import('./modes/loopOnce'),
+		loopRandom: () => import('./modes/loopRandom'),
+		scramble: () => import('./modes/scramble')
+	}
 
-    $: invalidCursorOnFinish && console.warn("[svelte-typewriter] The prop 'keepCursorOnFinish' is compatible only with finite modes")
-    $: invalidCursorOnDelay && console.warn("[svelte-typewriter] The prop 'showCursorOnDelay' has no effect if the delay is 0")
-    $: invalidLoopProps && console.warn("[svelte-typewriter] The props 'unwriteInterval' and 'wordInterval' are only compatible with loop modes")
-    $: invalidScrambleProps && console.warn("[svelte-typewriter] The props 'scrambleDuration' and 'scrambleSlowdown' are only compatible with scramble mode")
-    $: unnecessaryCursorOnFinish && console.warn("[svelte-typewriter] The prop 'keepCursorOnFinish' has no effect with values lower than 1")
+	run(() => {
+		invalidCursorOnFinish &&
+			console.warn(
+				"[svelte-typewriter] The prop 'keepCursorOnFinish' is compatible only with finite modes"
+			)
+	})
+	run(() => {
+		invalidCursorOnDelay &&
+			console.warn(
+				"[svelte-typewriter] The prop 'showCursorOnDelay' has no effect if the delay is 0"
+			)
+	})
+	run(() => {
+		invalidLoopProps &&
+			console.warn(
+				"[svelte-typewriter] The props 'unwriteInterval' and 'wordInterval' are only compatible with loop modes"
+			)
+	})
+	run(() => {
+		invalidScrambleProps &&
+			console.warn(
+				"[svelte-typewriter] The props 'scrambleDuration' and 'scrambleSlowdown' are only compatible with scramble mode"
+			)
+	})
+	run(() => {
+		unnecessaryCursorOnFinish &&
+			console.warn(
+				"[svelte-typewriter] The prop 'keepCursorOnFinish' has no effect with values lower than 1"
+			)
+	})
 
-    $: delayPromise = () => new Promise(resolve => setTimeout(() => resolve(delay), delay))
+	let delayPromise = $derived(
+		() => new Promise(resolve => setTimeout(() => resolve(delay), delay))
+	)
 
-    $: props = {
-        interval,
-        cursor,
-        keepCursorOnFinish,
-        delay,
-        showCursorOnDelay,
-        disabled,
-        element,
-        scrambleDuration,
-        scrambleSlowdown,
-        unwriteInterval,
-        wordInterval
-    }
+	let _props = $derived({
+		interval,
+		cursor,
+		keepCursorOnFinish,
+		delay,
+		showCursorOnDelay,
+		disabled,
+		element,
+		scrambleDuration,
+		scrambleSlowdown,
+		unwriteInterval,
+		wordInterval
+	})
 </script>
+
+<noscript>
+	{@render children?.()}
+</noscript>
+
+{#key _props}
+	{#if disabled}
+		<div class="typewriter-container">
+			{@render children?.()}
+		</div>
+	{:else}
+		{#await delayPromise()}
+			{#if showCursorOnDelay}
+				<div class="typewriter-container cursor">
+					<p class="typing"></p>
+				</div>
+			{/if}
+		{:then}
+			{#await modes[mode]() then selectedMode}
+				<svelte:element
+					this={element}
+					use:selectedMode.default={_props}
+					class:cursor
+					class="typewriter-container">
+					{@render children?.()}
+				</svelte:element>
+			{/await}
+		{/await}
+	{/if}
+{/key}
 
 <style>
 	@keyframes cursorFade {
@@ -83,39 +162,13 @@
 		content: none;
 	}
 
-    .cursor :global(.typing::after) {
-        content: '';
-        width: var(--cursor-width, 1ch);
-        height: 2ch;
-        display: inline-block;
-        vertical-align: text-top;
-        background-color: var(--cursor-color, #000000);
-        animation: cursorFade 1.25s infinite;
-    }
+	.cursor :global(.typing::after) {
+		content: '';
+		width: var(--cursor-width, 1ch);
+		height: 2ch;
+		display: inline-block;
+		vertical-align: text-top;
+		background-color: var(--cursor-color, #000000);
+		animation: cursorFade 1.25s infinite;
+	}
 </style>
-
-<noscript>
-    <slot />
-</noscript>
-
-{#key $$props}
-    {#if disabled}
-        <div class="typewriter-container">
-            <slot />
-        </div>
-    {:else}
-        {#await delayPromise()}
-            {#if showCursorOnDelay}
-                <div class="typewriter-container cursor">
-                    <p class="typing"></p>
-                </div>
-            {/if}
-        {:then}
-            {#await modes[mode]() then selectedMode}
-                <svelte:element this={element} use:selectedMode.default={props} class:cursor class="typewriter-container">
-                    <slot />
-                </svelte:element>
-            {/await}
-        {/await}
-    {/if}
-{/key}
